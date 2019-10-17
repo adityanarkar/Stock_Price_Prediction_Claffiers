0cb63982154fd5f845ec52a2052cfe3c26246399
Aditya Narkar <aditya.narkar25@gmail.com>
2019-10-17 17:58:59 -0500

    RF

--- main_fold.py
+++ main_fold.py
@@ -1,24 +1,18 @@
 import json
 import os
+import score
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import RFECV
-from sklearn.model_selection import TimeSeriesSplit, GridSearchCV, RandomizedSearchCV
-
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
+from data_prep import k_splits
 import definitions
 from data_prep import data_preparation as dp
 
 
-def getInitial():
-    initial = []
-    for i in range(3):
-        initial.append({"our_test_score": 0})
-    return initial
-
-
 def result_in_csv(STOCK, Algo, Estimator=0, Depth=0, Distance_function='0', No_of_features=0, Model_Score=0,
                   Future_day=0, C=-1,
-                  Our_test_score=-1):
+                  Our_test_score=-1.0):
     return f"{STOCK},{Algo},{Estimator},{Depth},{Distance_function},{No_of_features},{Model_Score},{Future_day},{C},{Our_test_score}\n "
 
 
@@ -39,45 +33,28 @@ def get_params_rf(estimator_start, estimator_stop, depth_start, depth_stop):
 
 def testRandomForests(STOCK, future_day, data_for_algos, estimator_start, estimator_stop,
                       depth_start, depth_stop, actual_data_to_predict):
-    result = ""
-    train_size = int(data_for_algos.shape[0] * 0.9)
-    print(train_size)
+    test_size = min(int(data_for_algos.shape[0] * 0.1), future_day)
     X = np.asarray(list(map(lambda row: row[:-1], data_for_algos)))
     y = np.asarray(list(map(lambda row: row[-1], data_for_algos)))
     parameters = get_params_rf(estimator_start, estimator_stop, depth_start, depth_stop)
-    print(parameters)
-    splits = get_splits(X)
-    print(splits)
     estimator = RandomForestClassifier()
-    # selector = RFECV(estimator, step=1, cv=TimeSeriesSplit(n_splits=5), scoring='accuracy')
+    # selector = RFECV(estimator, step=1, cv=TimeSeriesSplit(n_splits=3), scoring='accuracy')
     clf = GridSearchCV(estimator, param_grid=parameters, cv=TimeSeriesSplit(n_splits=5), n_jobs=-1, scoring='accuracy')
-    print(X.shape)
-    print(y.shape)
-    clf.fit(X[:train_size], y[:train_size])
-    # print(clf.best_estimator_.estimator_)
-    # print(clf.best_estimator_.grid_scores_)
-    # print(clf.best_estimator_.ranking_)
-    print(clf.best_params_)
-    print(clf.score(X[train_size:], y[train_size:]))
+    try:
+        clf.fit(X[:-test_size], y[:-test_size])
+    except:
+        return result_in_csv(STOCK, 'RF', Future_day=future_day)
+    max_depth, n_estimators, n_features = get_rf_clf_params(clf)
+    print(max_depth, n_estimators, n_features)
+    model_val_score = clf.best_score_
+    our_score = clf.score(X[-test_size:], y[-test_size:])
     print(clf.predict(actual_data_to_predict))
+    result = result_in_csv(STOCK, 'RF', n_estimators, max_depth, No_of_features=n_features, Future_day=future_day, Our_test_score=our_score, Model_Score=model_val_score)
     return result
 
 
-def get_top_rf(estimators, max_depth, model_score, future_day, no_of_features, last_test_score):
-    return {'estimators': estimators, 'max_depth': max_depth, 'model_score': model_score, 'future_day': future_day,
-            'no_of_features': no_of_features, 'our_test_score': last_test_score}
-
-
-def get_top_rf_result_csv_format(STOCK, top):
-    top_estimator = top["estimators"]
-    top_depth = top['max_depth']
-    top_model_score = top['model_score']
-    top_future_day = top['future_day']
-    top_no_of_features = top['no_of_features']
-    top_our_test_score = top['our_test_score']
-    result = result_in_csv(STOCK, 'RF', top_estimator, top_depth, No_of_features=top_no_of_features,
-                           Model_Score=top_model_score, Future_day=top_future_day, Our_test_score=top_our_test_score)
-    return result
+def get_rf_clf_params(clf):
+    return clf.best_estimator_.max_depth, clf.best_estimator_.n_estimators, clf.best_estimator_.n_features_
 
 
 def get_prepared_data(STOCK_FILE, window_size, feature_window_size, discretize):
@@ -109,13 +86,6 @@ def add_headers(RESULT_FILE):
     with open(RESULT_FILE, 'w') as f:
         f.write("Stock,Algorithm,Estimators,Depth,Distance_function,No_of_features,Model_Score,Future_day,"
                 "C,Our_test_score\n")
-
-
-def get_csv_result_knn(STOCK, top, future_day):
-    return result_in_csv(STOCK, 'KNN', Distance_function=top["distance_function"], Model_Score=top['score'],
-                         Future_day=future_day,
-                         Our_test_score=top['our_test_score'],
-                         No_of_features=top['no_of_features'])
 
 
 def runExperiment(lock, STOCK_FILE, RESULT_FILE, algos, future_day_start, future_day_stop, estimator_start,
@@ -177,7 +147,7 @@ for dictionary in configs:
     feature_window_size, discretize, C = get_config_from_dict(dictionary)
     pre_run_tasks(RESULT_FILE, COMPLETED_FILE)
 
-    files = list(map(lambda x: x.replace("\n", ""), open('55stocks.txt', 'r').readlines()))
+    files = list(map(lambda x: x.replace("\n", ""), open('1stock.txt', 'r').readlines()))
     files.reverse()
     print(files)
     for filename in files:
